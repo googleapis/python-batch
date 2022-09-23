d25143938995d7767986d658eb97d246dc98afb4
Maciej Strzelczyk <strzelczyk@google.com>
2022-09-23 16:39:05 +0200

    docs(samples): Adding sample for bucket mounting

--- samples/snippets/tests/test_basics.py
+++ samples/snippets/tests/test_basics.py
@@ -13,7 +13,7 @@
 #  limitations under the License.
 import time
 import uuid
-
+from typing import Callable
 
 import google.auth
 from google.cloud import batch_v1
@@ -44,7 +44,7 @@ def job_name():
     return f"test-job-{uuid.uuid4().hex[:10]}"
 
 
-def _test_body(test_job: batch_v1.Job):
+def _test_body(test_job: batch_v1.Job, additional_test: Callable = None):
     start_time = time.time()
     try:
         while test_job.status.state in WAIT_STATES:
@@ -61,6 +61,9 @@ def _test_body(test_job: batch_v1.Job):
                 break
         else:
             pytest.fail(f"Couldn't find job {test_job.uid} on the list of jobs.")
+
+        if additional_test:
+            additional_test()
     finally:
         delete_job(PROJECT, REGION, test_job.name.rsplit('/', maxsplit=1)[1]).result()
 
